94794f4222df5472cd49b4b8d1983c9facf1179b
joepvgenuchten <joep.van.genuchten@gmail.com>
2025-11-14 15:09:37 +0100

    updated gitignore

---
+++ src/simple_data_catalog/generate_data_catalog.py
@@ -0,0 +1,10 @@
+from simple_data_catalog.create_data_catalog import create_data_catalog
+import linkml
+
+def generate_data_catalog(
+        data_catalog_file: str= "data-catalog/data-catalog.yaml"):
+
+
+
+
+    return "done"
